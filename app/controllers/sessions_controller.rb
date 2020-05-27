80bd743c3e404db2927770a55c03074389d5fd52
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-27 18:08:52 +0900

    Mod SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    if user.save
+      cookies.encrypted[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path
+    end
   end
 
   def destroy
+    cookies.delete(:user_id)
+    redirect_to new_session_path
   end
 end
