d68db947ca2636dc4afc606d2e348322f4329797
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-27 18:09:21 +0900

    helper_method :current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    User.find(cookies.encrypted[:user_id]) if cookies.encrypted[:user_id]
+  end
 end
